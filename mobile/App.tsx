1d78589dcb9364c0ddde91dc9e6459baafbfa614
dedojs <xandresousax@gmail.com>
2022-09-14 14:32:43 -0300

    feat: stage 2 - mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,42 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
+import { Background } from './src/componentes/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/componentes/Loading';
 
 export default function App() {
+
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>Olá André</Text>
-      <Text>Iniciando Projeto nlw-eSports</Text>
-      <Text>Preparar para decolagem do foguete! ...3...2...1</Text>
-      <Text>Iniciar Decolagem! Boa Sorte</Text>
-      <Button title='Decolagem!' style={styles.button} />
-      <StatusBar style="auto" />
-    </View>
+    <Background >
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      { fontsLoaded ? <Home /> : <Loading /> }
+    </Background>
   );
 }
 
-interface ButtonProps {
-  title: string;
-  style: object;
-}
 
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#90ee90',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: '#fff',
-    marginTop: '15px',
-  }
-});
